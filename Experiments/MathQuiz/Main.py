533f15c5998be9fe0ab18c47cf2075349176c2c4
raddox7 <robotrocke11@gmail.com>
2020-10-17 23:10:59 +0700

    Session 2 - Updated the math quiz game

--- Experiments/MathQuiz/Main.py
+++ Experiments/MathQuiz/Main.py
@@ -1,6 +1,6 @@
 from Experiments.MathQuiz.DataBank import Questions
 
-rounds = int(input("How many rounds you wish to play: "))
+rounds = int(input("How many rounds you wish to play (Set to '-1' for unlimited): "))
 chance = int(input("How many chances are you gonna be allowed to re-answer a question: "))
 
 game = Questions(rounds, chance)
